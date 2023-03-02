f2ac8daa491e0348e309c2c447f7d6f3a70ebf42
Isabel Palacios <issabelpalacios@gmail.com>
2023-03-02 16:24:22 -0300

    Create repo

---
+++ main.js
@@ -0,0 +1,16 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const email = document.querySelector('#email').value;
+  const password = document.querySelector('#password').value;
+
+  localStorage.setItem('user', JSON.stringify({ email, password }));
+
+  swal('¡Inicio de sesión exitoso!', '', 'success')
+    .then(() => {
+      window.location.href = 'trivia.html';
+    });
+});
+
